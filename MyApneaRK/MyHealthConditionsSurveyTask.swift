fa2a69d929aa4ab91bceef599082410e63f4b66e
Kyle Rand <krand7@gmail.com>
2015-06-25 11:59:11 -0400

    Added 'My Sleep Quality' and 'My Health Conditions' survey tasks, for #1

--- MyApneaRK/MyHealthConditionsSurveyTask.swift
+++ MyApneaRK/MyHealthConditionsSurveyTask.swift
@@ -13,11 +13,47 @@ public var MyHealthConditionsSurveyTask: ORKOrderedTask {
     
     var steps = [ORKStep]()
     
+    // Instructions
     let instructionStep = ORKInstructionStep(identifier: "IntroStep")
     instructionStep.title = "My Health Conditions Survey"
     instructionStep.text = ""
     steps += [instructionStep]
     
+    // Health conditions
+    let healthConditionsFormStep = ORKFormStep(identifier: "healthConditionsFormStep", title: "Health Conditions", text: "Has a doctor or health care professional ever told you that you had any of the following?")
+    let healthConditionsFormChoices = [
+        ORKTextChoice(text: "Yes", value: 0),
+        ORKTextChoice(text: "No", value: 1),
+        ORKTextChoice(text: "Don't know", value: 2)
+    ]
+    let healthConditionsFormAnswerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: healthConditionsFormChoices)
+    var healthConditionsFormItems = [ORKFormItem]()
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition1", text: "Allergies", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition2", text: "Asthma", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition3", text: "Attention Deficit Disorder (ADD)", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition4", text: "Attention Deficit Hyperactivity Disorder (ADHD)", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition5", text: "Cancer", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition6", text: "Chronic Obstructive Lung Disease (COPD or Emphysema)", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition7", text: "Depression", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition8", text: "Diabetes", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition9", text: "Epilepsy or Seizure Disorder", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition10", text: "High Blood Pressure", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition11", text: "Heart Disease", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition12", text: "Insomnia", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition13", text: "Narcolepsy", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition14", text: "Pulmonary Fibrosis", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition15", text: "Restless Leg Syndrome", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormItems += [ORKFormItem(identifier: "healthcondition16", text: "Stroke", answerFormat: healthConditionsFormAnswerFormat)]
+    healthConditionsFormStep.formItems = healthConditionsFormItems
+    steps += [healthConditionsFormStep]
+    
+    // Other health conditions
+    let otherConditionsQuestionStepTitle = "If you've been diagnosed with other significant health conditions, please list them here."
+    let otherConditionsAnswerFormat = ORKAnswerFormat.textAnswerFormat()
+    let otherConditionsQuestionStep = ORKQuestionStep(identifier: "otherConditionsQuestionStep", title: otherConditionsQuestionStepTitle, answer: otherConditionsAnswerFormat)
+    steps += [otherConditionsQuestionStep]
+    
+    // Summary
     let summaryStep = ORKCompletionStep(identifier: "SummaryStep")
     summaryStep.title = "Congratulations!"
     summaryStep.text = "You have completed the My Health Conditions survey! Thank you for contributing your data. You have been entered into our monthly drawing for a $250 Visa gift card."
--- MyApneaRK/MySleepQualitySurveyTask.swift
+++ MyApneaRK/MySleepQualitySurveyTask.swift
@@ -13,11 +13,46 @@ public var MySleepQualitySurveyTask: ORKOrderedTask {
     
     var steps = [ORKStep]()
     
+    // Instructions
     let instructionStep = ORKInstructionStep(identifier: "IntroStep")
     instructionStep.title = "My Sleep Quality Survey"
     instructionStep.text = ""
     steps += [instructionStep]
     
+    // Recent sleep quality
+    let recentSleepQualityQuestionStepTitle = "In the past 7 days, my sleep quality was:"
+    let recentSleepQualityChoices = [
+        ORKTextChoice(text: "Very poor", value: 0),
+        ORKTextChoice(text: "Poor", value: 1),
+        ORKTextChoice(text: "Fair", value: 2),
+        ORKTextChoice(text: "Good", value: 3),
+        ORKTextChoice(text: "Very good", value: 4)
+    ]
+    let recentSleepQualityAnswerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: recentSleepQualityChoices)
+    let recentSleepQualityQuestionStep = ORKQuestionStep(identifier: "RecentSleepQualityStep", title: recentSleepQualityQuestionStepTitle, answer: recentSleepQualityAnswerFormat)
+    steps += [recentSleepQualityQuestionStep]
+    
+    // Likelihood of dozing
+    let sleepQualityFormStep = ORKFormStep(identifier: "SleepQualityFormStep", title: "Recent Sleep", text: "Thinking about your sleep in the past 7 days, please answer the following questions.")
+    let sleepQualityFormChoices = [
+        ORKTextChoice(text: "High chance of dozing", value: 0),
+        ORKTextChoice(text: "Moderate chance of dozing", value: 1),
+        ORKTextChoice(text: "Slight chance of dozing", value: 2),
+        ORKTextChoice(text: "Would never doze", value: 3)
+    ]
+    let sleepQualityFormAnswerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: sleepQualityFormChoices)
+    var sleepQualityFormItems = [ORKFormItem]()
+    sleepQualityFormItems += [ORKFormItem(identifier: "sleepquality1", text: "My sleep was refreshing.", answerFormat: sleepQualityFormAnswerFormat)]
+    sleepQualityFormItems += [ORKFormItem(identifier: "sleepquality2", text: "I had a problem with my sleep.", answerFormat: sleepQualityFormAnswerFormat)]
+    sleepQualityFormItems += [ORKFormItem(identifier: "sleepquality3", text: "I had difficulty falling asleep.", answerFormat: sleepQualityFormAnswerFormat)]
+    sleepQualityFormItems += [ORKFormItem(identifier: "sleepquality4", text: "My sleep was restless.", answerFormat: sleepQualityFormAnswerFormat)]
+    sleepQualityFormItems += [ORKFormItem(identifier: "sleepquality5", text: "I tried hard to get sleep.", answerFormat: sleepQualityFormAnswerFormat)]
+    sleepQualityFormItems += [ORKFormItem(identifier: "sleepquality6", text: "I worried about not being able to fall asleep.", answerFormat: sleepQualityFormAnswerFormat)]
+    sleepQualityFormItems += [ORKFormItem(identifier: "sleepquality7", text: "I was satisfied with my sleep.", answerFormat: sleepQualityFormAnswerFormat)]
+    sleepQualityFormStep.formItems = sleepQualityFormItems
+    steps += [sleepQualityFormStep]
+    
+    // Summary
     let summaryStep = ORKCompletionStep(identifier: "SummaryStep")
     summaryStep.title = "Congratulations!"
     summaryStep.text = "You have completed the My Sleep Quality survey! Thank you for contributing your data. You have been entered into our monthly drawing for a $250 Visa gift card."
