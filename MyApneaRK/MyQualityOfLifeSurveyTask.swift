1699bb65aa6d56f5b2cff259fa5e001c8da06e2d
Kyle Rand <krand7@gmail.com>
2015-06-25 13:11:41 -0400

    Fleshed out 'QOL' and 'Risk Profile' survey tasks, for #1

--- MyApneaRK/MyQualityOfLifeSurveyTask.swift
+++ MyApneaRK/MyQualityOfLifeSurveyTask.swift
@@ -11,13 +11,122 @@ import ResearchKit
 
 public var MyQualityOfLifeSurveyTask: ORKOrderedTask {
     
+    // This survey uses a similar subset of text choices, so initializing them here
+    let ratingChoices = [
+        ORKTextChoice(text: "Poor", value: 0),
+        ORKTextChoice(text: "Fair", value: 1),
+        ORKTextChoice(text: "Good", value: 2),
+        ORKTextChoice(text: "Very good", value: 3),
+        ORKTextChoice(text: "Excellent", value: 4)
+    ]
+    let ratingAnswerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: ratingChoices)
+    
+    let comparisonChoices = [
+        ORKTextChoice(text: "Much worse than one year ago", value: 0),
+        ORKTextChoice(text: "Somewhat worse now than one year ago", value: 1),
+        ORKTextChoice(text: "About the same as one year ago", value: 2),
+        ORKTextChoice(text: "Somewhat better now than one year ago", value: 3),
+        ORKTextChoice(text: "Much better now than one year ago", value: 4)
+    ]
+    let comparisonAnswerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: comparisonChoices)
+    
+    let extentChoices = [
+        ORKTextChoice(text: "Not at all", value: 0),
+        ORKTextChoice(text: "A little", value: 1),
+        ORKTextChoice(text: "Moderately", value: 2),
+        ORKTextChoice(text: "Mostly", value: 3),
+        ORKTextChoice(text: "Completely", value: 4)
+    ]
+    let extentAnswerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: extentChoices)
+    
+    let difficultyChoices = [
+        ORKTextChoice(text: "Unable to do", value: 0),
+        ORKTextChoice(text: "With much difficulty", value: 1),
+        ORKTextChoice(text: "With some difficulty", value: 2),
+        ORKTextChoice(text: "With a little difficulty", value: 3),
+        ORKTextChoice(text: "Without any difficulty", value: 4)
+    ]
+    let difficultyAnswerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: difficultyChoices)
+    
+    let intensityChoices = [
+        ORKTextChoice(text: "Very much", value: 0),
+        ORKTextChoice(text: "Quite a bit", value: 1),
+        ORKTextChoice(text: "Somewhat", value: 2),
+        ORKTextChoice(text: "A little bit", value: 3),
+        ORKTextChoice(text: "Not at all", value: 4)
+    ]
+    let intensityAnswerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: intensityChoices)
+    
+    let frequencyChoices = [
+        ORKTextChoice(text: "Always", value: 0),
+        ORKTextChoice(text: "Often", value: 1),
+        ORKTextChoice(text: "Sometimes", value: 2),
+        ORKTextChoice(text: "Rarely", value: 3),
+        ORKTextChoice(text: "Never", value: 4)
+    ]
+    let frequencyAnswerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: frequencyChoices)
+    
+    
     var steps = [ORKStep]()
     
+    // Instructions
     let instructionStep = ORKInstructionStep(identifier: "IntroStep")
     instructionStep.title = "My Quality of Life Survey"
     instructionStep.text = ""
     steps += [instructionStep]
     
+    // Health rating - general
+    let healthRatingQuestionStepTitle = "In general, how would you rate your health?"
+    let healthRatingQuestionStep = ORKQuestionStep(identifier: "HealthRating", title: healthRatingQuestionStepTitle, answer: ratingAnswerFormat)
+    steps += [healthRatingQuestionStep]
+    
+    // Health rating - improvement
+    let healthImprovementQuestionStepTitle = "Compared to one year ago, how would you rate your health in general now?"
+    let healthImprovementQuestionStep = ORKQuestionStep(identifier: "HealthImprovement", title: healthImprovementQuestionStepTitle, answer: comparisonAnswerFormat)
+    steps += [healthImprovementQuestionStep]
+    
+    // QOL rating - general
+    let qolRatingQuestionStepTitle = "In general, how would you rate your quality of life?"
+    let qolRatingQuestionStep = ORKQuestionStep(identifier: "QOLRating", title: qolRatingQuestionStepTitle, answer: ratingAnswerFormat)
+    steps += [qolRatingQuestionStep]
+    
+    // Physica activities
+    let physicalActivitiesQuestionStepTitle = "To what extent are you able to carry out your everyday physical activities such as walking, climbing stairs, carrying groceries, or moving a chair?"
+    let physicalActivitiesQuestionStep = ORKQuestionStep(identifier: "PhysicalActivities", title: physicalActivitiesQuestionStepTitle, answer: extentAnswerFormat)
+    steps += [physicalActivitiesQuestionStep]
+    
+    // Errands
+    let errandsQuestionStepTitle = "How well are you able to run errands and shop?"
+    let errandsQuestionStep = ORKQuestionStep(identifier: "Errands", title: errandsQuestionStepTitle, answer: difficultyAnswerFormat)
+    steps += [errandsQuestionStep]
+    
+    // Pain
+    let painQuestionStepTitle = "In the past 7 days, how much did pain interfere with your day to day activities?"
+    let painQuestionStep = ORKQuestionStep(identifier: "PainInterferance", title: painQuestionStepTitle, answer: intensityAnswerFormat)
+    steps += [painQuestionStep]
+    
+    // Depression
+    let depressedQuestionStepTitle = "In the past 7 days, I felt depressed:"
+    let depressedQuestionStep = ORKQuestionStep(identifier: "Depression", title: depressedQuestionStepTitle, answer: frequencyAnswerFormat)
+    steps += [depressedQuestionStep]
+    
+    // Fatigue
+    let fatigueQuestionStepTitle = "During the past 7 days, I felt fatigued:"
+    let fatigueQuestionStep = ORKQuestionStep(identifier: "Fatigue", title: fatigueQuestionStepTitle, answer: intensityAnswerFormat)
+    steps += [fatigueQuestionStep]
+    
+    // Sleep
+    let sleepQuestionStepTitle = "In the past 7 days, I had a problem with my sleep:"
+    let sleepQuestionStep = ORKQuestionStep(identifier: "SleepProblems", title: sleepQuestionStepTitle, answer: intensityAnswerFormat)
+    steps += [sleepQuestionStep]
+    
+    // Leisure
+    let leisureQuestionStepTitle = "I have trouble doing all of my regular leisure activities with others:"
+    let leisureQuestionStep = ORKQuestionStep(identifier: "Leisure", title: leisureQuestionStepTitle, answer: frequencyAnswerFormat)
+    steps += [leisureQuestionStep]
+
+    
+    // Summary
     let summaryStep = ORKCompletionStep(identifier: "SummaryStep")
     summaryStep.title = "Congratulations!"
     summaryStep.text = "You have completed the My Quality of Life survey! Thank you for contributing your data. You have been entered into our monthly drawing for a $250 Visa gift card."
--- MyApneaRK/MyRiskProfileSurveyTask.swift
+++ MyApneaRK/MyRiskProfileSurveyTask.swift
@@ -13,11 +13,41 @@ public var MyRiskProfileSurveyTask: ORKOrderedTask {
     
     var steps = [ORKStep]()
     
+    // Instructions
     let instructionStep = ORKInstructionStep(identifier: "IntroStep")
     instructionStep.title = "My Risk Profile Survey"
     instructionStep.text = ""
     steps += [instructionStep]
     
+    // Symptoms
+    let symptomsFormStepTitle = "Do you currently experience any of these symptoms? How long has each symptom been present?"
+    let symptomsFormStep = ORKFormStep(identifier: "Symptoms", title: "Symptoms", text: symptomsFormStepTitle)
+    let symptomsChoices = [
+        ORKTextChoice(text: "0-3 months", value: 0),
+        ORKTextChoice(text: "3-6 months", value: 1),
+        ORKTextChoice(text: "6-12 months", value: 2),
+        ORKTextChoice(text: "12-24 months", value: 3),
+        ORKTextChoice(text: "N/A", value: 4)
+    ]
+    let symptomsAnswerFormat = ORKAnswerFormat.choiceAnswerFormatWithStyle(.SingleChoice, textChoices: symptomsChoices)
+    var symptomsFormItems = [ORKFormItem]()
+    symptomsFormItems += [ORKFormItem(identifier: "symptom1", text: "Snoring", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom2", text: "Sleepiness", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom3", text: "Tiredness", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom4", text: "Driving or work accident", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom5", text: "Shortness of breath during sleep", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom6", text: "Stopped breathing during sleep", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom7", text: "Heart disease", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom8", text: "High blood pressure", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom9", text: "Depressed mood", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom10", text: "Irritability", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom11", text: "Concerned friend or spouse", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom12", text: "Forgetfulness", answerFormat: symptomsAnswerFormat)]
+    symptomsFormItems += [ORKFormItem(identifier: "symptom13", text: "Healthcare provider's suggestion", answerFormat: symptomsAnswerFormat)]
+    symptomsFormStep.formItems = symptomsFormItems
+    steps += [symptomsFormStep]
+    
+    // Summary
     let summaryStep = ORKCompletionStep(identifier: "SummaryStep")
     summaryStep.title = "Congratulations!"
     summaryStep.text = "You have completed the My Risk Profile survey! Thank you for contributing your data. You have been entered into our monthly drawing for a $250 Visa gift card."
