c6e20673e60f841849f55ceb699bc431da0d99b9
Kyle Rand <krand7@gmail.com>
2015-07-14 13:40:48 -0400

    Initialized display of forum topics with basic info, for #6

---
+++ MyApneaRK/ForumTableViewController.swift
@@ -0,0 +1,144 @@
+//
+//  ForumTableViewController.swift
+//  MyApneaRK
+//
+//  Created by Kyle Rand on 7/14/15.
+//  Copyright (c) 2015 myapnea. All rights reserved.
+//
+
+import UIKit
+
+class ForumTableViewController: UITableViewController {
+    
+    var forumTopics:NSArray = [NSDictionary]()
+    var topicSelectedId = Int()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Initialize table view
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.estimatedRowHeight = 86.0
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        // Retrieve forum data
+        let url = NSURL(string: "https://staging.partners.org/myapnea.org/api/forums/topics.json")
+        let session = NSURLSession.sharedSession()
+        let request = NSURLRequest(URL: url!)
+        let task = session.dataTaskWithURL(url!, completionHandler: {
+            (data, response, error) -> Void in
+            
+            if (error != nil) {
+                println(error)
+            } else {
+                let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
+                self.forumTopics = jsonResult.valueForKey("topics") as! NSArray
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.tableView.reloadData()
+                    self.tableView.setNeedsLayout()
+                    self.tableView.layoutIfNeeded()
+                    self.tableView.reloadData()
+                })
+            }
+            
+        })
+        
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return forumTopics.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TopicCell", forIndexPath: indexPath) as! UITableViewCell
+
+        let topic = forumTopics[indexPath.row] as! NSDictionary
+        cell.textLabel!.text = topic.valueForKey("name") as? String
+
+        return cell
+    }
+
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let topic = forumTopics[indexPath.row] as! NSDictionary
+        self.topicSelectedId = topic.valueForKey("id") as! Int
+        self.performSegueWithIdentifier("topicShowSegue", sender: nil)
+        
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "topicShowSegue" {
+            let destination = segue.destinationViewController as! TopicViewController
+            destination.topicId = self.topicSelectedId as Int
+            
+        }
+    }
+    
+
+}
