68536539d5389521d1e6378d742bfb31d621c8b2
Kyle Rand <krand7@gmail.com>
2015-07-14 14:41:02 -0400

    Topic show now conforms to UITableViewController, for #6

--- MyApneaRK/TopicShowTableViewController.swift
+++ MyApneaRK/TopicShowTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ForumTableViewController.swift
+//  TopicShowTableViewController.swift
 //  MyApneaRK
 //
 //  Created by Kyle Rand on 7/14/15.
@@ -8,26 +8,19 @@
 
 import UIKit
 
-class ForumTableViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    var forumTopics:NSArray = [NSDictionary]()
-    var topicSelectedId = Int()
+class TopicShowTableViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var topicId = Int()
+    var posts = NSArray()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Initialize table view
         self.tableView.delegate = self
         self.tableView.dataSource = self
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
-        // Retrieve forum data
-        let url = NSURL(string: "https://staging.partners.org/myapnea.org/api/forums/topics.json")
+        // Get topic from API
+        println(topicId)
+        let url = NSURL(string: "https://staging.partners.org/myapnea.org/api/forums/topics/\(topicId).json")
         let session = NSURLSession.sharedSession()
         let request = NSURLRequest(URL: url!)
         let task = session.dataTaskWithURL(url!, completionHandler: {
@@ -37,7 +30,7 @@ class ForumTableViewController: UITableViewController, UITableViewDelegate, UITa
                 println(error)
             } else {
                 let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
-                self.forumTopics = jsonResult.valueForKey("topics") as! NSArray
+                self.posts = jsonResult.valueForKey("posts") as! NSArray
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                     self.tableView.reloadData()
                     self.tableView.setNeedsLayout()
@@ -49,6 +42,7 @@ class ForumTableViewController: UITableViewController, UITableViewDelegate, UITa
         })
         
         task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -67,26 +61,17 @@ class ForumTableViewController: UITableViewController, UITableViewDelegate, UITa
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return forumTopics.count
+        return posts.count
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("TopicCell", forIndexPath: indexPath) as! UITableViewCell
-        let topic = forumTopics[indexPath.row] as! NSDictionary
-        cell.textLabel!.text = topic.valueForKey("name") as? String
+        let cell = tableView.dequeueReusableCellWithIdentifier("postCell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel!.text = posts[indexPath.row].valueForKey("description") as? String
         return cell
     }
-
-
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
-        let topic = forumTopics[indexPath.row] as! NSDictionary
-        self.topicSelectedId = topic.valueForKey("id") as! Int
-        self.performSegueWithIdentifier("showTopicSegue", sender: nil)
-        
-    }
     
+
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
@@ -122,19 +107,14 @@ class ForumTableViewController: UITableViewController, UITableViewDelegate, UITa
     }
     */
 
-    
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
-        if segue.identifier == "showTopicSegue" {
-            let destination = segue.destinationViewController as! TopicShowTableViewController
-            destination.topicId = self.topicSelectedId as Int
-            
-        }
     }
-    
+    */
 
 }
